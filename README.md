0992f33bd1019fb5958a1d08579cbd9f5c9a7ea5
Alisamar Husain <zrthxn@gmail.com>
2024-06-11 01:11:03 +0200

    Initial Setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# Backend
+
+## Setup
+
+0. Clone this Repository
+
+1. Create a virutal environment and activate it
+```bash
+virtualenv .venv -p python3.10
+source .venv/bin/activate
+```
+
+2. Install requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ routes/users.py
@@ -0,0 +1,55 @@
+"""
+User Routes
+-----------
+
+- CREATE user
+- READ user
+- UPDATE user
+- DELETE user
+- CREATE user_profile
+- READ user_profile
+- UPDATE user_profile
+- DELETE user_profile
+"""
+
+from fastapi import APIRouter, Request
+
+
+router = APIRouter()
+
+
+@router.get("/users/")
+async def read_users(request: Request):
+    """ Read all users
+    """
+    ...
+
+
+@router.get("/users/{username}")
+async def read_user(username: str):
+    """ Read user by username
+
+    Args:
+        username (uuid): User ID
+    """
+    ...
+
+
+@router.put("/users/{username}")
+async def update_user(username: str, request: Request):
+    """ Read user by username
+
+    Args:
+        username (uuid): User ID
+    """
+    ...
+    
+    
+@router.delete("/users/{username}")
+async def delete_user(username: str):
+    """ Read user by username
+
+    Args:
+        username (uuid): User ID
+    """
+    ...

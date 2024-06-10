5afbd14898c5bad76cbaf84941050fc0e9d408f8
Abdul Rafay <rafayrana036@gmail.com>
2024-06-11 01:57:04 +0200

    Ran the fastapi server

--- routes/users.py
+++ routes/users.py
@@ -20,7 +20,7 @@ router = APIRouter()
 
 @router.get("/")
 async def read_users(request: Request):
-    return JSONResponse({"response": "Welcome to Tehelka App"})
+    return {"response": "Welcome to Tehelka App"}
 
 
 @router.get("/{username}")

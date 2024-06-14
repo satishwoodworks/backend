0e5f181b2a1f4f91dc24fe71b07573f01de77d62
Alisamar Husain <zrthxn@gmail.com>
2024-06-14 02:21:33 +0200

    CORS Middleware (#8)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 ENV = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(ENV, override = True)
@@ -10,6 +11,14 @@ from server.routes import users, login
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 @app.get("/")
 def startup():
